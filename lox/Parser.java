3c31b754214dab61c32ecde4c35c2f671a6ac5b8
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-10-07 15:57:36 +0200

    add parse and synchronize methods

--- lox/Parser.java
+++ lox/Parser.java
@@ -16,6 +16,14 @@ class Parser {
         this.tokens = tokens;
     }
 
+    Expr parse() {
+        try {
+            return expression();
+        } catch (ParseError error) {
+            return null;
+        }
+    }
+
     private Expr expression() {
         return equality();
     }
@@ -86,7 +94,7 @@ class Parser {
             consume(RIGHT_PAREN, "Expect ')' after epxression");
             return new Expr.Grouping(expr);
         }
-        return null;
+        throw error(peek(), "Expected expression");
     }
 
     private boolean match(TokenType... types) {
@@ -133,5 +141,28 @@ class Parser {
     private ParseError error(Token token, String message) {
         Lox.error(token, message);
         return new ParseError();
+    }
+
+    private void synchronize() {
+        advance();
+
+        while (!isAtEnd()) {
+            if (previous().type == SEMICOLON)
+                return;
+            switch (peek().type) {
+                case CLASS:
+                case FUN:
+                case VAR:
+                case FOR:
+                case IF:
+                case WHILE:
+                case PRINT:
+                case RETURN:
+                    return;
+                default:
+                    ;
+            }
+            advance();
+        }
     }
 }
