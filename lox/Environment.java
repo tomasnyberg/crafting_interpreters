c0309601460e403468113b3d77b0eb5d233e467f
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-10-12 14:21:48 +0200

    add block statements and block scoping

--- lox/Environment.java
+++ lox/Environment.java
@@ -3,7 +3,16 @@ package lox;
 import java.util.HashMap;
 
 class Environment {
+    final Environment enclosing;
     private final HashMap<String, Object> values = new HashMap<>();
+
+    Environment() {
+        this.enclosing = null;
+    }
+
+    Environment(Environment enclosing) {
+        this.enclosing = enclosing;
+    }
 
     void define(String name, Object value) {
         values.put(name, value);
@@ -13,12 +22,21 @@ class Environment {
         if (values.containsKey(name.lexeme)) {
             return values.get(name.lexeme);
         }
+
+        if (enclosing != null)
+            return enclosing.get(name);
+
         throw new RuntimeError(name, "Undefined variable: " + name.lexeme);
     }
 
     void assign(Token name, Object value) {
         if (values.containsKey(name.lexeme)) {
             values.put(name.lexeme, value);
+            return;
+        }
+
+        if (enclosing != null) {
+            enclosing.assign(name, value);
             return;
         }
 
--- lox/Parser.java
+++ lox/Parser.java
@@ -1,6 +1,7 @@
 package lox;
 
 import java.util.List;
+import java.util.ArrayList;
 
 import static lox.TokenType.*;
 
@@ -43,6 +44,8 @@ class Parser {
     private Stmt statement() {
         if (match(PRINT))
             return printStatement();
+        if (match(LEFT_BRACE))
+            return new Stmt.Block(block());
         return expressionStatement();
     }
 
@@ -68,6 +71,15 @@ class Parser {
         Expr expr = expression();
         consume(SEMICOLON, "Expect ; after expression");
         return new Stmt.Expression(expr);
+    }
+
+    private List<Stmt> block() {
+        List<Stmt> statements = new ArrayList<>();
+        while (!check(RIGHT_BRACE) && !isAtEnd()) {
+            statements.add(declaration());
+        }
+        consume(RIGHT_BRACE, "Expect } after block.");
+        return statements;
     }
 
     private Expr assignment() {
