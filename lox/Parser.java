0725208198fe9fa0ed61a7eebb43566ab06462fe
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-10-12 15:49:17 +0200

    add logical AND and OR

--- lox/Parser.java
+++ lox/Parser.java
@@ -61,7 +61,6 @@ class Parser {
             elseBranch = statement();
         }
         return new Stmt.If(condition, thenBranch, elseBranch);
-
     }
 
     private Stmt printStatement() {
@@ -98,7 +97,7 @@ class Parser {
     }
 
     private Expr assignment() {
-        Expr expr = equality();
+        Expr expr = or();
 
         if (match(EQUAL)) {
             Token equals = previous();
@@ -111,6 +110,30 @@ class Parser {
 
             error(equals, "Invalid assignment target.");
         }
+        return expr;
+    }
+
+    private Expr or() {
+        Expr expr = and();
+
+        while (match(OR)) {
+            Token operator = previous();
+            Expr right = and();
+            expr = new Expr.Logical(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    private Expr and() {
+        Expr expr = equality();
+
+        while (match(AND)) {
+            Token operator = previous();
+            Expr right = equality();
+            expr = new Expr.Logical(expr, operator, right);
+        }
+
         return expr;
     }
 
