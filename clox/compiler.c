ba61c9f0b8a9a34c78460a5dd322d1d090945d90
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-12-12 10:29:45 +0100

    chap 23: implement for loop fully; initializer, condition and increment

--- clox/compiler.c
+++ clox/compiler.c
@@ -368,15 +368,48 @@ static void expressionStatement() {
 }
 
 static void forStatement() {
+  beginScope();
   consume(TOKEN_LEFT_PAREN, "Expect '(' after 'for'");
-  consume(TOKEN_SEMICOLON, "Expect ';'");
+  if (match(TOKEN_SEMICOLON)) {
+    // Nothhing in first slot of for loop
+  } else if (match(TOKEN_VAR)) {
+    varDeclaration();
+  } else {
+    expressionStatement();
+  }
 
   int loopStart = currentChunk()->count;
-  consume(TOKEN_SEMICOLON, "Expect ';'");
-  consume(TOKEN_RIGHT_PAREN, "Expect ')' after for clauses");
+  int exitJump = -1;
+  if (!match(TOKEN_SEMICOLON)) {
+    expression();
+    consume(TOKEN_SEMICOLON, "Expect ';' after loop condition");
+
+    // exit condition of loop
+    exitJump = emitJump(OP_JUMP_IF_FALSE);
+    emitByte(OP_POP);
+  }
+
+  if (!match(TOKEN_RIGHT_PAREN)) {
+    int bodyJump = emitJump(OP_JUMP);
+    int incrementStart = currentChunk()->count;
+    expression();
+    emitByte(OP_POP);
+    consume(TOKEN_RIGHT_PAREN, "Expect ')' after for clause");
+
+    emitLoop(loopStart);
+    loopStart = incrementStart;
+    patchJump(bodyJump);
+  }
 
   statement();
   emitLoop(loopStart);
+
+  if (exitJump != -1) {
+    patchJump(exitJump);
+    emitByte(OP_POP);
+  }
+
+  endScope();
 }
 
 static void ifStatement() {
