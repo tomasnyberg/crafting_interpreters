2729e196617e7e9a8367d5866a3dea42999557fa
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-12-16 15:42:07 +0100

    chap 25: add runtime support for closures

--- clox/memory.c
+++ clox/memory.c
@@ -21,6 +21,8 @@ void *reallocate(void *pointer, size_t oldSize, size_t newSize) {
 static void freeObject(Obj *object) {
   switch (object->type) {
   case OBJ_CLOSURE: {
+    ObjClosure *closure = (ObjClosure *)object;
+    FREE_ARRAY(ObjUpvalue *, closure->upvalues, closure->upvalueCount);
     FREE(ObjClosure, object);
     break;
   }
@@ -40,6 +42,9 @@ static void freeObject(Obj *object) {
     FREE(ObjString, object);
     break;
   }
+  case OBJ_UPVALUE:
+    FREE(ObjUpvalue, object);
+    break;
   }
 }
 
