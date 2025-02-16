2d5216489bddbd190f323f5bcfaa9b0306c556a6
tomasnyberg <tomas.nyberg7335@gmail.com>
2025-02-16 13:01:36 +0100

    chap 28: keep track of current class in compiler to disallowd bad 'this' usage

--- clox/compiler.c
+++ clox/compiler.c
@@ -67,8 +67,13 @@ typedef struct Compiler {
   int scopeDepth;
 } Compiler;
 
+typedef struct ClassCompiler {
+  struct ClassCompiler *enclosing;
+} ClassCompiler;
+
 Parser parser;
 Compiler *current = NULL;
+ClassCompiler *currentClass = NULL;
 
 static Chunk *currentChunk() { return &current->function->chunk; }
 
@@ -515,12 +520,20 @@ static void classDeclaration() {
   declareVariable();
   emitBytes(OP_CLASS, nameConstant);
   defineVariable(nameConstant);
+
+  ClassCompiler classCompiler;
+  classCompiler.enclosing = currentClass;
+  currentClass = &classCompiler;
+
   namedVariable(className, false);
   consume(TOKEN_LEFT_BRACE, "Expect '{' before class body.");
   while (!check(TOKEN_RIGHT_BRACE) && !check(TOKEN_EOF)) {
     method();
   }
   consume(TOKEN_RIGHT_BRACE, "Expect '}' after class body.");
+  emitByte(OP_POP);
+
+  currentClass = currentClass->enclosing;
 }
 
 static void funDeclaration() {
@@ -759,7 +772,13 @@ static void variable(bool canAssign) {
   namedVariable(parser.previous, canAssign);
 }
 
-static void this_(bool canAssign) { variable(false); }
+static void this_(bool canAssign) {
+  if (currentClass == NULL) {
+    error("Can't use 'this' outside of a class");
+    return;
+  }
+  variable(false);
+}
 
 static void unary(bool canAssign) {
   TokenType operatorType = parser.previous.type;
